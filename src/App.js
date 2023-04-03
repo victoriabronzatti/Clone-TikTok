564bd10589efdd79f16921d027459238accbd5c1
Victoria Bronzatti <victoriamacedo1801@gmail.com>
2023-04-03 15:33:44 -0300

    conection with Firebase

--- src/App.js
+++ src/App.js
@@ -1,28 +1,41 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import Video from "./pages/Video";
+import db from "./config/firebase";
+import { collection, getDocs } from "firebase/firestore/lite";
 
 function App() {
+  const [video, setVideos] = useState([]);
+
+  async function getVideos() {
+    const videosCollection = collection(db, "videos");
+    const videosSnapshot = await getDocs(videosCollection);
+    const videosList = videosSnapshot.docs.map((doc) => doc.data());
+    setVideos(videosList);
+  }
+
+  useEffect(() => {
+    getVideos();
+  }, []);
+
   return (
     <div className="App">
       <div className="app__videos">
-        <Video
-          likes={100}
-          messages={200}
-          shares={300}
-          name="Victória Bronzatti"
-          description="Kitten"
-          music="Music epic"
-          url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4"
-        />
-        <Video 
-        likes={432} 
-        messages={232} 
-        shares={115} 
-        name="Victória "
-        description="Kitty"
-        music="Meow"
-        url="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/bird.mp4"
-        />
+        {video.map((item) => {
+          return (
+            <Video
+              likes={item.likes}
+              messages={item.messages}
+              shares={item.shares}
+              name={item.name}
+              description={item.description}
+              music={item.music}
+              url={item.url}
+            />
+          );
+        })}
+
+
       </div>
     </div>
   );
